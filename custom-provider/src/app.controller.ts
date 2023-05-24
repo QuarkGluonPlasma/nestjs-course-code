e9e054c71ff72cc4174b4471dcec014d31b30e32
guang <quark_gluon_plasma@163.com>
2023-05-24 09:41:41 +0800

    refector: custom-provider add property inject

--- custom-provider/src/app.controller.ts
+++ custom-provider/src/app.controller.ts
@@ -3,16 +3,32 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private  readonly appService: AppService,
-    @Inject('app_service') private readonly appService2: AppService,
-    @Inject('person') private readonly person: {name: string, age: number},
-    @Inject('person2') private readonly person2: {name: string, desc: string},
-    @Inject('person3') private readonly person3: {name: string, desc: string},
-    @Inject('person4') private readonly person4: {name: string, age: number},
-    @Inject('person5') private readonly person5: {name: string, desc: string},
+  // constructor(
+  //   private  readonly appService: AppService,
+  //   @Inject('app_service') private readonly appService2: AppService,
+  //   @Inject('person') private readonly person: {name: string, age: number},
+  //   @Inject('person2') private readonly person2: {name: string, desc: string},
+  //   @Inject('person3') private readonly person3: {name: string, desc: string},
+  //   @Inject('person4') private readonly person4: {name: string, age: number},
+  //   @Inject('person5') private readonly person5: {name: string, desc: string},
 
-  ) {}
+  // ) {}
+
+  @Inject(AppService)
+  private  readonly appService: AppService;
+  @Inject('app_service')
+  private readonly appService2: AppService;
+  @Inject('person')
+  private readonly person: {name: string, age: number};
+  @Inject('person2')
+  private readonly person2: {name: string, desc: string};
+  @Inject('person3') 
+  private readonly person3: {name: string, desc: string};
+  @Inject('person4')
+  private readonly person4: {name: string, age: number};
+  @Inject('person5')
+  private readonly person5: {name: string, desc: string};
+
 
   @Get()
   getHello(): string {
