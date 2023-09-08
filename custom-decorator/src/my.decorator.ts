17286ddc404a0251ce2d717cc19a6ba3b594d994
guang <quark_gluon_plasma@163.com>
2023-09-09 00:02:24 +0800

    improve: custom-decorator MyHeaders 不区分大小写

--- custom-decorator/src/my.decorator.ts
+++ custom-decorator/src/my.decorator.ts
@@ -4,7 +4,7 @@ import { Request } from 'express';
 export const MyHeaders = createParamDecorator(
   (key: string, ctx: ExecutionContext) => {
     const request: Request = ctx.switchToHttp().getRequest();
-    return key ? request.headers[key] : request.headers;
+    return key ? request.headers[key.toLowerCase()] : request.headers;
   },
 );
 
