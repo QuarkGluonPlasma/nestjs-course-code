be4ced38cd824f6438009206bdfe641759307d49
guang <quark_gluon_plasma@163.com>
2023-09-09 16:46:02 +0800

    improve:five-transmission-method  useStaticAssets 直接写目录名

--- five-transmission-method/src/main.ts
+++ five-transmission-method/src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'public'), {
+  app.useStaticAssets('public', {
     prefix: '/static'
   });
   await app.listen(3000);
