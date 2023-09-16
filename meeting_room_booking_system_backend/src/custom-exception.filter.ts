5f858c9e297da5f624ff684b2c4f0957e2c02614
guang <quark_gluon_plasma@163.com>
2023-09-16 15:23:58 +0800

    feat: meeting-room-booking-system-backend 自定义异常过滤器兼容 ValidationPipe 的错误格式

--- meeting_room_booking_system_backend/src/custom-exception.filter.ts
+++ meeting_room_booking_system_backend/src/custom-exception.filter.ts
@@ -6,10 +6,13 @@ export class CustomExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse<Response>();
     response.statusCode = exception.getStatus();
+
+    const res = exception.getResponse() as { message: string[] };
+    
     response.json({
       code: exception.getStatus(),
       message: 'fail',
-      data: exception.message
+      data: res?.message?.join ? res?.message?.join(',') : exception.message
     }).end();
   }
 }
