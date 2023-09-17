c6e2b111327f4a6c22372ed16c5b0fccdcc2b5e0
guang <quark_gluon_plasma@163.com>
2023-09-17 08:14:01 +0800

    feat: meeting-room-booking-system-backend 会议室管理模块 描述和设备 IsNotEmpty

--- meeting_room_booking_system_backend/src/meeting-room/dto/create-meeting-room.dto.ts
+++ meeting_room_booking_system_backend/src/meeting-room/dto/create-meeting-room.dto.ts
@@ -23,11 +23,17 @@ export class CreateMeetingRoomDto {
     })
     location: string;
 
+    @IsNotEmpty({
+        message: '设备不能为空'
+    })
     @MaxLength(50, {
         message: '设备最长为 50 字符'
     })
     equipment: string;
 
+    @IsNotEmpty({
+        message: '描述不能为空'
+    })
     @MaxLength(100, {
         message: '描述最长为 100 字符'
     })
