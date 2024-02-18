a4ef3febaf78b7e2c72253cc9a8514ca50fb1b01
guang <quark_gluon_plasma@163.com>
2024-02-18 12:44:31 +0800

    feat: meeting_room_booking_system_backend 添加 docker compose

---
+++ meeting_room_booking_system_backend/Dockerfile
@@ -0,0 +1,29 @@
+FROM node:18.0-alpine3.14 as build-stage
+
+WORKDIR /app
+
+COPY package.json .
+
+RUN npm config set registry https://registry.npmmirror.com/
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+# production stage
+FROM node:18.0-alpine3.14 as production-stage
+
+COPY --from=build-stage /app/dist /app
+COPY --from=build-stage /app/package.json /app/package.json
+
+WORKDIR /app
+
+RUN npm config set registry https://registry.npmmirror.com/
+
+RUN npm install --production
+
+EXPOSE 3005
+
+CMD ["node", "/app/main.js"]
--- meeting_room_booking_system_backend/src/app.module.ts
+++ meeting_room_booking_system_backend/src/app.module.ts
@@ -18,6 +18,7 @@ import { MeetingRoom } from './meeting-room/entities/meeting-room.entity';
 import { BookingModule } from './booking/booking.module';
 import { Booking } from './booking/entities/booking.entity';
 import { StatisticModule } from './statistic/statistic.module';
+import * as path from 'path';
 
 @Module({
   imports: [
@@ -36,7 +37,8 @@ import { StatisticModule } from './statistic/statistic.module';
     UserModule, 
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: 'src/.env'
+      // envFilePath: 'src/.env'
+      envFilePath: path.join(__dirname, '.env')
     }),
     TypeOrmModule.forRootAsync({
       useFactory(configService: ConfigService) {
