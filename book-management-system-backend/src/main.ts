fc99a26d39671a21917812e5e22231e31ee41bdb
guang <quark_gluon_plasma@163.com>
2024-07-17 23:37:45 +0800

    improve: book-management-system-backend

--- book-management-system-backend/src/main.ts
+++ book-management-system-backend/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true}));
+
+  app.enableCors();
+
+  app.useStaticAssets(join(__dirname, '../uploads'), {prefix: '/uploads'});
 
   await app.listen(3000);
 }
